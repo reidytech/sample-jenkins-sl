fd782aceba92adf67c6c7d34725f4c07c5c376fc
Chris Reidy <reidy.data@gmail.com>
2022-03-23 19:41:06 -0400

    testing checkout

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,4 +2,13 @@
 
 def call(String name = 'human') {
     echo "Hello, ${name}."
+    dir("test app") {
+        checkout(
+            [
+                $class: 'GitSCM',
+                brances: [[name: master]],
+                userRemoteConfigs: [[url: 'https://github.com/reidytech/sample-jenkins-pipeline']]
+            ]
+        )
+    }
 }
