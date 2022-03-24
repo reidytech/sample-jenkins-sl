b15069d1d902933c8c0e32b457f29420961e62c6
Chris Reidy <reidy.data@gmail.com>
2022-03-24 09:12:12 -0400

    testing map

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,23 +1,26 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-    echo "Hello, ${name}."
-    dir("test app") {
-        checkout(
-            [
-                $class: 'GitSCM',
-                branches: [[name: 'master']],
-                userRemoteConfigs: [[url: 'https://github.com/reidytech/sample-jenkins-pipeline']]
-            ]
-        )
-    }
-    def scriptOutput = sh(
-        script: '''
-            #!/bin/bash
-            pwd
-            ls
-        '''
-    )
-    echo "Checking out SCM ONCE again"
-    checkout scm
+def call(Map m) {
+    // echo "Hello, ${name}."
+    // dir("test app") {
+    //     checkout(
+    //         [
+    //             $class: 'GitSCM',
+    //             branches: [[name: 'master']],
+    //             userRemoteConfigs: [[url: 'https://github.com/reidytech/sample-jenkins-pipeline']]
+    //         ]
+    //     )
+    // }
+    // def scriptOutput = sh(
+    //     script: '''
+    //         #!/bin/bash
+    //         pwd
+    //         ls
+    //     '''
+    // )
+    // echo "Checking out SCM ONCE again"
+    // checkout scm
+    echo "${m.TERRAGUARD}"
+    echo "${m.APP_NAME}"
+
 }
